b44d5967d6e18e341ff9287da7bed97f98553278
Justin Flory <jflory7@gmail.com>
2013-07-25 16:52:35 -0400

    Minor changes

--- src/com/crystalcraftmc/witherbow/Main.java
+++ src/com/crystalcraftmc/witherbow/Main.java
@@ -7,16 +7,12 @@ public final class Main extends JavaPlugin
 	@Override
 	public void onEnable()
 	{
-        // TODO Insert logic to be performed when the plugin is enabled
-		getLogger().info("WitherBow has been enabled!");
-		
 		getServer().getPluginManager().registerEvents(new BowListener(this), this);
 	}
 	
 	@Override
 	public void onDisable()
 	{
-        // TODO Insert logic to be performed when the plugin is enabled
-		getLogger().info("WitherBow has been enabled!");
+        // Add a way to clear the the string enabledPlayers?
 	}
 }
