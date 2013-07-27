acce1b865b0218033cd728605b2c3514645c9d22
Justin Flory <jflory7@gmail.com>
2013-07-27 16:55:02 -0400

    Added auto-updater and stats

--- src/com/crystalcraftmc/witherbow/Main.java
+++ src/com/crystalcraftmc/witherbow/Main.java
@@ -1,5 +1,7 @@
 package com.crystalcraftmc.witherbow;
 
+import java.io.IOException;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin
@@ -7,6 +9,19 @@ public final class Main extends JavaPlugin
 	@Override
 	public void onEnable()
 	{
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException e){
+			// Failed to submit the stats :-(
+		}
+		
+        if (this.getConfig().getBoolean("auto-update"))
+        {
+        	@SuppressWarnings("unused")
+			Updater updater = new Updater(this, "wither-bow", this.getFile(), Updater.UpdateType.DEFAULT, true);
+        }
+		
 		BowListener bl = new BowListener(this);
 		
         getCommand("witherbow").setExecutor(bl);
